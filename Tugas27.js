602eceaeac3bfdd0bc9b22a90ad4a95563887871
Alghifari Muhammad Dzar <alghifari.m.d@gmail.com>
2020-01-04 21:42:47 +0300

    Tugas 27 JS
    Tugas ini untuk Niomic

---
+++ Tugas27.js
@@ -0,0 +1,13 @@
+function tugas27() {
+var alamat2 = ["Jalan pangeran Tirtayasa No", 70];
+var alamat1 = ["No", " rumah saya ", 30,"A"];
+
+hasil = alamat1.join().split(',');
+hasil1 = hasil.join("");
+console.log(hasil1);
+
+hasil2 = alamat2.join(' ');
+console.log(hasil2);
+}
+
+tugas27();
